e9eaec0216833480ba1c57f9f5eefb8d7cd3dc82
Avishekh Sinha (Avi) <39235012+avishekhsinhaRepo@users.noreply.github.com>
2023-12-19 09:56:54 +0530

    vs code web app deployment

---
+++ Azurenode/routes/profile.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const router = express.Router();
+const axios = require("axios");
+const axiosRetry = require("axios-retry");
+
+router.get("/", async (req, res, next) => {
+    // axiosRetry(axios, {
+    //     retries: 3,
+    //     onRetry : () => { console.error('Axios retry started at :' + new Date())}, 
+    //     shouldResetTimeout: true,
+    //     });
+    try{
+    const response = await axios.get(
+  {
+      "userId": 1,
+      "id": 1,
+      "title": "delectus aut autem",
+      "completed": false
+  },{timeout : Number(5)});
+        res.send(JSON.stringify(response.data))
+  }catch(error){
+    // if (axios.isAxiosError(error)) {
+    //     const axiosError = error ;
+    //     console.log(axiosError.message);
+    //     console.log(axiosError.response);
+    // }
+    console.error("************************************************")
+    console.error(error)
+  }
+ 
+})
+module.exports = router;
